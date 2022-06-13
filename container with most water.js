50e5e3edb82ac143d7a08cd934ed730f5436e65b
Rabiya <100783343+Rabiya-kaira@users.noreply.github.com>
2022-06-13 10:37:48 +0530

    Add files via upload

---
+++ container with most water.js
@@ -0,0 +1,17 @@
+var maxArea = function(height) {
+    let maximumArea = Number.MIN_SAFE_INTEGER;
+    let left = 0;
+    let right = height.length - 1;
+    while (left < right) {
+        let shorterLine = Math.min(height[left], height[right]);
+        maximumArea = Math.max(maximumArea, shorterLine * (right - left));
+        if (height[left] < height[right]) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return maximumArea;
+     
+    
+};
